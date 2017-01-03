5956c7d652aedb00bfa0a5f21a25b99ddfa7972d
Stavros Filippidis <SFilippidis@users.noreply.github.com>
2017-01-03 23:04:41 +0200

    Changes for version 1.0.2

--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,6 @@
 // Header file for the main window.
 
-// Copyright (C) 2011-2015 Stavros Filippidis
+// Copyright (C) 2011-2017 Stavros Filippidis
 // Contact: stavros@filippidis.name
 
 // This file is part of QGreatstWeightCalculator.
