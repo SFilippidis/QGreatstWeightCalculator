38619e1d76f98f8f583fbbda082f2f8f79a1a94e
Stavros Filippidis <SFilippidis@users.noreply.github.com>
2020-01-07 21:57:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // The main function of the application.
 
-// Copyright (C) 2011-2018 Stavros Filippidis
+// Copyright (C) 2011-2020 Stavros Filippidis
 // Contact: sfilippidis@gmail.com
 
 // This file is part of QGreatstWeightCalculator.
