f6f45e11aeb0c42613d9100cacc9a8d5b5c9f82b
Stavros Filippidis <SFilippidis@users.noreply.github.com>
2021-02-06 15:27:25 +0200

    New email

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // The main function of the application.
 
 // Copyright (C) 2011-2021 Stavros Filippidis
-// Contact: sfilippidis@gmail.com
+// Contact: stavros@filippidis.name
 
 // This file is part of QGreatstWeightCalculator.
 
